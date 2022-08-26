e4d4d1d23eb070a07359dfc581ec07091ecafad5
Amitabh Sarkar <icodingbad@gmail.com>
2022-08-27 00:31:16 +0530

    add MainActivityViewModel to update and getcount

--- app/src/main/java/com/amitabh/kotlinmvvm/MainActivity.kt
+++ app/src/main/java/com/amitabh/kotlinmvvm/MainActivity.kt
@@ -2,10 +2,21 @@ package com.amitabh.kotlinmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.amitabh.kotlinmvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.countText.text = viewModel.getCurrentCount().toString()
+        binding.button.setOnClickListener {
+
+            binding.countText.text = viewModel.getUpdatedCount().toString()
+        }
     }
 }
