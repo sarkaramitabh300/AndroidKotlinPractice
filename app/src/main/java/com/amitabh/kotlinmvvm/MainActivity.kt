a6b3e781b59b6509d990f8627218b9da05a11dfc
Amitabh Sarkar <icodingbad@gmail.com>
2022-08-26 23:55:51 +0530

    bind data directly in the xml file using variable

--- app/src/main/java/com/amitabh/kotlinmvvm/MainActivity.kt
+++ app/src/main/java/com/amitabh/kotlinmvvm/MainActivity.kt
@@ -2,10 +2,19 @@ package com.amitabh.kotlinmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.amitabh.kotlinmvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.student = getStudent()
+
+    }
+
+    private fun getStudent(): Student {
+        return Student(1, "Alex", "alex@gmail.com")
     }
 }
---
+++ app/src/main/java/com/amitabh/kotlinmvvm/Student.kt
@@ -0,0 +1,7 @@
+package com.amitabh.kotlinmvvm
+
+data class Student(
+    var id: Int,
+    var name: String,
+    var email: String
+)
