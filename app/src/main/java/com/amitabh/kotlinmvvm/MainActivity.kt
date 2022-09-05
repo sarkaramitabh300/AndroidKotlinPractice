b13705406ec75db2e2f414973af9c54102118d2c
Amitabh Sarkar <amitabhs>
2022-09-05 15:33:12 +0530

    changes

--- app/src/main/java/com/amitabh/kotlinmvvm/MainActivity.kt
+++ app/src/main/java/com/amitabh/kotlinmvvm/MainActivity.kt
@@ -2,6 +2,11 @@ package com.amitabh.kotlinmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
